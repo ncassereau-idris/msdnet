310baa8f59f0e90676d26a604e5d5970b49ac0b3
Nathan Cassereau <nathan.cassereau@idris.fr>
2022-07-08 11:13:00 +0200

    SSIM

--- msdnet/loss.py
+++ msdnet/loss.py
@@ -14,6 +14,9 @@
 
 from . import operations
 import abc
+import torch
+import torch.nn.functional as F
+import math
 import numpy as np
 
 class Loss(abc.ABC):
@@ -56,6 +59,85 @@ class Loss(abc.ABC):
         '''
         pass
 
+
+class SSIM(Loss, torch.nn.Module):
+
+    def __init__(self, window_size=11, channel=1, size_average=True, sigma=1.5, C1=0.01**2, C2=0.03**2):
+        super(SSIM, self).__init__()
+        self.window_size = window_size
+        self.size_average = size_average
+        self.channel = channel
+        self.sigma = sigma
+        self.C1 = C1
+        self.C2 = C2
+        self.padding = self.window_size // 2
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.window = self.create_window(window_size, channel).to(self.device)
+
+    def gaussian(self, window_size):
+        gauss = torch.Tensor([
+            math.exp(-(x - window_size//2) ** 2 / float(2 * self.sigma ** 2))
+            for x in range(window_size)
+        ])
+        return gauss/gauss.sum()
+
+    def create_window(self, window_size, channel):
+        _1D_window = self.gaussian(window_size).unsqueeze(1)
+        _2D_window = _1D_window.mm(_1D_window.t()).float().unsqueeze(0).unsqueeze(0)
+        window = _2D_window.expand(channel, 1, window_size, window_size).contiguous()
+        return window
+
+    def conv2d(self, X):
+        return F.conv2d(
+            X,
+            self.window,
+            padding=self.padding,
+            groups=self.channel
+        )
+
+    def ssim(self, img1, img2):
+        mu1 = self.conv2d(img1)
+        mu2 = self.conv2d(img2)
+
+        mu1_sq = mu1.pow(2)
+        mu2_sq = mu2.pow(2)
+
+        mu1_mu2 = mu1 * mu2
+
+        sigma1_sq = self.conv2d(img1 * img1) - mu1_sq
+        sigma2_sq = self.conv2d(img2 ** 2) - mu2_sq
+
+        sigma12 = self.conv2d(img1 * img2) - mu1_mu2
+
+        U = (2 * mu1_mu2 + self.C1) * (2 * sigma12 + self.C2)
+        V = (mu1_sq + mu2_sq + self.C1) * (sigma1_sq + sigma2_sq + self.C2)
+        ssim_map = U / V
+
+        loss = torch.clamp(1. - ssim_map, min=0, max=1) / 2.
+
+        if self.size_average:
+            return loss.mean()
+        else:
+            return loss.mean((1, 2, 3))
+
+    def np2torch(self, image):
+        return torch.from_numpy(image).to(self.device)[None] # Add a batch dimension
+
+    def loss(self, im, tar):
+        with torch.no_grad():
+            im = self.np2torch(im)
+            tar = self.np2torch(tar)
+            output = self.ssim(im, tar).detach().cpu().numpy()
+        return output
+
+    def deriv(self, im, tar):
+        im = self.np2torch(im).requires_grad_()
+        tar = self.np2torch(tar)
+        output = self.ssim(im, tar)
+        grad, = torch.autograd.grad(output, im)
+        return grad[0].cpu().numpy() # remove batch dimension and convert to numpy
+
+    forward = loss
 
 class L2Loss(Loss):
     '''Computes L2-norm loss function.
